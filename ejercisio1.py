bf98a5a69e9e3070c3d9259d1be91747ad3181a6
jose martin <mcarmonapalestina@gmail.com>
2024-10-10 09:55:07 -0600

    01 Primer ejercisio

---
+++ ejercisio1.py
@@ -0,0 +1,12 @@
+
+"""
+Ejercisio 1:
+Escribir un programa que le pregunte al usuario su nombre e imprima un saludo
+Entradas:
+nombre: string
+Salida:
+saludo: string
+"""
+nombre = input("Escribe tu nombre:")
+saludo = "Hola" + nombre + "!"
+print(saludo)
